e80bc09961247bb11f70fce4281a67778966f83e
Daniel Kurek <daniel.kurek.dev@gmail.com>
2024-09-29 19:23:41 +0200

    [mhp50] update pins

--- source/Core/BSP/MHP50/Pins.h
+++ source/Core/BSP/MHP50/Pins.h
@@ -10,9 +10,9 @@
 #include "configuration.h"
 
 // MHP50 pin map
-#define KEY_A_Pin                     GPIO_PIN_8
+#define KEY_A_Pin                     GPIO_Pin_8
 #define KEY_A_GPIO_Port               GPIOC
-#define KEY_B_Pin                     GPIO_PIN_9
+#define KEY_B_Pin                     GPIO_Pin_9
 #define KEY_B_GPIO_Port               GPIOC
 // #define TMP36_INPUT_Pin               GPIO_PIN_1
 // #define TMP36_INPUT_GPIO_Port         GPIOB
@@ -26,12 +26,26 @@
 // #define VIN_GPIO_Port                 GPIOA
 // #define VIN_ADC1_CHANNEL              ADC_CHANNEL_1
 // #define VIN_ADC2_CHANNEL              ADC_CHANNEL_1
+#define SPI_MISO_PIN                  GPIO_Pin_6
+#define SPI_MISO_PORT                 GPIOA
+#define SPI_MOSI_PIN                  GPIO_Pin_7
+#define SPI_MOSI_PORT                 GPIOA
+#define SPI_SCLK_PIN                  GPIO_Pin_5
+#define SPI_SCLK_PORT                 GPIOA
+#define FLASH_SPI_CS_PIN              GPIO_Pin_14
+#define FLASH_SPI_CS_PORT             GPIOB
+#define LCD_SPI_CS_PIN                GPIO_Pin_4
+#define LCD_SPI_CS_PORT               GPIOA
+#define LCD_SPI_DC_PIN                GPIO_Pin_5
+#define LCD_SPI_DC_PORT               GPIOD
+#define LCD_SPI_RES_PIN               GPIO_Pin_6
+#define LCD_SPI_RES_PORT              GPIOD
 // #define OLED_RESET_Pin                GPIO_PIN_4
 // #define OLED_RESET_GPIO_Port          GPIOB
-#define PWM_Out_Pin                   GPIO_PIN_1
+#define PWM_Out_Pin                   GPIO_Pin_1
 #define PWM_Out_GPIO_Port             GPIOB
 #define PWM_Out_CHANNEL               TIM_CHANNEL_4
-#define BUZZER_Pin                    GPIO_PIN_8
+#define BUZZER_Pin                    GPIO_Pin_8
 #define BUZZER_GPIO_Port              GPIOB
 #define BUZZER_CHANNEL                TIM_CHANNEL_3
 // #define SCL_Pin                       GPIO_PIN_6
@@ -44,7 +58,7 @@
 // #define SDA2_GPIO_Port                GPIOA
 // #define INT_PD_Pin                    GPIO_PIN_5
 // #define INT_PD_GPIO_Port              GPIOB
-#define HEAT_EN_Pin                   GPIO_PIN_13
+#define HEAT_EN_Pin                   GPIO_Pin_13
 #define HEAT_EN_GPIO_Port             GPIOB
 // #define PLATE_SENSOR_PULLUP_Pin       GPIO_PIN_1
 // #define PLATE_SENSOR_PULLUP_GPIO_Port GPIOD
@@ -54,6 +68,6 @@
 // #define PLATE_SENSOR_ADC1_CHANNEL ADC_CHANNEL_5
 // #define PLATE_SENSOR_ADC2_CHANNEL ADC_CHANNEL_5
 
-#define WS2812_Pin       GPIO_PIN_15 // TODO: check if it is WS2812 compatible LED
+#define WS2812_Pin       GPIO_Pin_15 // TODO: check if it is WS2812 compatible LED
 #define WS2812_GPIO_Port GPIOB
 #endif /* BSP_MINIWARE_PINS_H_ */
